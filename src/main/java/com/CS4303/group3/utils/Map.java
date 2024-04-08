258cf3a3d1786f4510e3eabb23797f11bf2e7215
Ewan1245 <ewan.purves@gmail.com>
2024-04-08 13:52:54 +0100

    Revert "Added player start position to level creator"
    This reverts commit c013afe0860b39a5bfeb5d193c29e1ee5356d487.

--- src/main/java/com/CS4303/group3/utils/Map.java
+++ src/main/java/com/CS4303/group3/utils/Map.java
@@ -11,12 +11,10 @@ import processing.core.PVector;
 public class Map {
     //variables need to be public for jackson to convert to JSON correctly
     public List<Ground_Tile> ground_tiles;
-    public PVector player_position;
 
 
     public Map() {
         ground_tiles = new ArrayList<>();
-        player_position = new PVector(0,0);
     }
 
     public void add_ground_tile(PVector position, PVector size) {
@@ -30,12 +28,9 @@ public class Map {
     public void draw(Level_Creator lc) {
         //draw all the ground tiles
         lc.background(0);
-        lc.fill(255);
         for(Ground_Tile gt : ground_tiles) {
             lc.rect(gt.position.x, gt.position.y, gt.size.x, gt.size.y);
         }
-        lc.fill(128);
-        lc.rect(player_position.x, player_position.y, lc.playerWidth, lc.playerHeight);
     }
 
 
