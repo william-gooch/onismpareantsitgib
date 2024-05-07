71482e6b6208d73e26409d835c25a4f262660160
William Gooch <william.gooch.wg@gmail.com>
2024-05-07 12:42:14 +0100

    Add bouncy and player block sprites

--- assets/tiles.tsx
+++ assets/tiles.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tiles" tilewidth="16" tileheight="16" tilecount="24" columns="6">
- <image source="tiles-new.png" width="96" height="64"/>
+<tileset version="1.10" tiledversion="1.10.2" name="tiles" tilewidth="16" tileheight="16" tilecount="36" columns="6">
+ <image source="tiles-new.png" width="96" height="96"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="16" height="16"/>
@@ -116,6 +116,16 @@
  <tile id="23">
   <objectgroup draworder="index" id="2">
    <object id="1" x="5" y="0" width="6" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
 </tileset>
